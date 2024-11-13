5bc2426285f5b7b62e6aff2f91e09765a55c58b3
r4isen1920 <me@r4isen.net>
2024-11-13 09:00:51 +0800

    Add files via upload

---
+++ _06_LaboratoryExercise_1_RaisenCadavez_BSIT101.java
@@ -0,0 +1,18 @@
+package _06_laboratoryexercise_1_raisencadavez_bsit.pkg101;
+
+public class _06_LaboratoryExercise_1_RaisenCadavez_BSIT101 {
+    public static void main(String[] args) {
+        byte iNumber = 3;
+
+        if (iNumber >= 0) {
+            if (iNumber == 0) {
+                System.out.println("first string");
+            }
+            else {
+                System.out.println("Second string");
+                System.out.println("third string");
+            }
+        }
+    }
+
+}
